627894fc8a3d4387fc8731e0f613a59292e50deb
Syed Khawar <93276430+SyedKhawarNiaz@users.noreply.github.com>
2024-05-21 00:32:47 +0500

    done

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ img.addEventListener("mouseenter", () => {
   cursor.innerHTML = "View More";
   gsap.to(cursor, {
     scale: 2,
+    opacity : 1,
     backgroundImage: "linear-gradient(60deg, #29323c 0%, #485563 100%)",
     duration: 0.5,
     ease: "power1.out",
@@ -30,6 +31,7 @@ img.addEventListener("mouseleave", () => {
   cursor.innerHTML = "";
   gsap.to(cursor, {
     scale: 1,
+    opacity : 0,
     backgroundImage:
       "radial-gradient(73% 147%, #EADFDF 59%, #ECE2DF 100%), radial-gradient(91% 146%, rgba(255,255,255,0.50) 47%, rgba(0,0,0,0.50) 100%)",
   });

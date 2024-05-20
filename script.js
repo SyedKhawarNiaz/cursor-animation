d6b5f3d1e88f10ec477947c4bacdaeb43682f6dd
Syed Khawar <93276430+SyedKhawarNiaz@users.noreply.github.com>
2024-05-21 00:28:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function locomotive() {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    lerb: 0.01,
+    smooth: true,
+  });
+}
+// locomotive();
+let page1 = document.querySelector(".main");
+let cursor = document.querySelector(".cursor");
+let img = document.querySelector(".img");
+page1.addEventListener("mousemove", (val) => {
+  gsap.to(cursor, {
+    x: val.clientX,
+    y: val.clientY,
+    duration: 0.5,
+    ease: "power1.out",
+  });
+});
+img.addEventListener("mouseenter", () => {
+  cursor.innerHTML = "View More";
+  gsap.to(cursor, {
+    scale: 2,
+    backgroundImage: "linear-gradient(60deg, #29323c 0%, #485563 100%)",
+    duration: 0.5,
+    ease: "power1.out",
+  });
+});
+img.addEventListener("mouseleave", () => {
+  cursor.innerHTML = "";
+  gsap.to(cursor, {
+    scale: 1,
+    backgroundImage:
+      "radial-gradient(73% 147%, #EADFDF 59%, #ECE2DF 100%), radial-gradient(91% 146%, rgba(255,255,255,0.50) 47%, rgba(0,0,0,0.50) 100%)",
+  });
+});
